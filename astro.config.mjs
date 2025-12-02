621c32bc3bd20377b243949a8a145ecb3f2a82a1
chrisauri0 <chaparitoveb20@gmail.com>
2025-12-02 12:35:22 -0600

    add SEO and Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), ],
+  site: "https://zentry.sh" ,
+  integrations: [svelte(), sitemap()],
 
 
 });
